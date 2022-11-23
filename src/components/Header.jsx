b2d823dbca58cc717702ed0bfb50b1d292bb935d
Pavel <pavel.holod2001@gmail.com>
2022-11-23 20:16:47 +0300

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img src='https://images.vexels.com/media/users/3/137349/isolated/preview/7b8717630942d7fb84ae3c74b0602c0f-heart-logo-minimalism.png' />
+            <p>ВОТ ОНИ СЛЕВА НАПРАВО</p>
+        </header>
+    )
+}
+
+
+export default Header
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+function Navigation() {
+    return (
+        <nav className='nav'>
+            <div><a href=''></a>Profile</div>
+            <div><a href=''></a>Messages</div>
+            <div><a href=''></a>News</div>
+            <div><a href=''></a>Music</div>
+            <div><a href=''></a>Setings</div>
+        </nav>
+    )
+}
+
+
+export default Navigation
